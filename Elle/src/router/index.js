860bc9754b0507dc2feb55afbd306ec228e6e42d
RYChiy <85196512+RYChiy@users.noreply.github.com>
2022-12-26 21:10:39 +0100

    added

--- Elle/src/router/index.js
+++ Elle/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Login from "../views/LogIn.vue";
+import Homepage from "../views/homepage.vue"
 const routes = [
     {
-      path: "/",
-      name: "login",
+      path: "/login",
+      name: "Login",
       component: Login,
-    }]
+    },
+  {
+    path: "/h",
+    name: "Homepage",
+    component: Homepage
+  }]
 
     const router = createRouter({
         history: createWebHashHistory(),
---
+++ Elle/src/views/homepage.vue
@@ -0,0 +1,14 @@
+<template>
+
+  <h1>GFUTEN tag=""</h1>
+   
+</template>
+ 
+ <script>
+
+ </script>
+ 
+ <style>
+ 
+ 
+ </style>
